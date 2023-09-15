ef2c40859f049d12ebbb4a7951f7756403294c47
Christopher Sepúlveda <christopher172003@outlook.cl>
2023-09-15 12:26:33 -0300

    Se completa reto 01.

---
+++ apuntes/javascript/03.variables.js
@@ -0,0 +1,6 @@
+let V = 0;
+if (V) {
+    console.log("verdadero");
+} else {
+    console.log("falso");
+}
---
+++ apuntes/javascript/05.prompt.js
@@ -0,0 +1,5 @@
+// let edad = prompt("¿Cuál es tu edad?", 0);
+// alert(`Tu edad es ${edad}`);
+
+let question = confirm("confirmar");
+alert(`valor ${question}`);
