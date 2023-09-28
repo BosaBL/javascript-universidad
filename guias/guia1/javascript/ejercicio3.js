e3031282ccc2599c63c421eeed8cafcc3b9bd809
Christopher <christopher172003@outlook.cl>
2023-09-28 01:11:01 -0300

    Se finaliza guia 1

--- guias/guia1/javascript/ejercicio3.js
+++ guias/guia1/javascript/ejercicio3.js
@@ -1,35 +1,45 @@
 function check_password(password) {
-    let error_list = [];
-    let reg_special_chars = /(?=.*[!@#$%^&*])/;
-    let reg_number = /^[0-9]+$/;
-    let reg_upper_case_letter = /[A-Z]/;
+  let error_list = [];
 
-    if (!test(reg_special_chars)) {
-        error_list.unshift("Falta algún caracter especial");
-    } else if (!test(reg_number)) {
-        error_list.unshift("Falta algún numero.");
-    } else if (!test(reg_upper_case_letter)) {
-        error_list.ushift("Falta alguna letra mayúscula");
-    }
+  // expresiones regulares para matchear al menos una aparicion de; caracteres especiales, numeros y letras mayúsculas
+  let reg_special_chars = /(?=.*[!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|])/;
+  let reg_number = /[0-9]/;
+  let reg_upper_case_letter = /[A-Z]/;
 
-    if (error_list.length === 0) {
-        return true;
-    }
+  if (!reg_special_chars.test(password)) {
+    error_list.push("Falta algún caracter especial");
+  }
+  if (!reg_number.test(password)) {
+    error_list.push("Falta algún numero.");
+  }
+  if (!reg_upper_case_letter.test(password)) {
+    error_list.push("Falta alguna letra mayúscula");
+  }
+  if (password.length < 8) {
+    error_list.push(
+      "La contraseña debe tener al menos 8 caracteres de longitud.",
+    );
+  }
 
-    return error_list;
+  if (error_list.length === 0) {
+    return true;
+  }
+  return error_list;
 }
 
 function main() {
-    let password_sec = false;
-    let password;
-    while (password_sec === false) {
-        password = prompt("Ingrese su contraseña.");
-        let pass_sec = check_password(password);
-        if (pass_sec === true) {
-            alert("Su contraseña es segura.");
-            password_sec = true;
-        } else {
-            message = "Su cotraseña no es segura: \n" + pass_sec.join("\n");
-        }
+  let password_sec = false;
+  let password;
+  while (password_sec === false) {
+    password = prompt("Ingrese su contraseña.");
+    let pass_sec = check_password(password);
+    if (pass_sec === true) {
+      alert("Su contraseña es segura.");
+      password_sec = true;
+    } else {
+      message = "Su cotraseña no es segura: \n\n\n" + pass_sec.join("\n");
+      alert(message);
     }
+  }
 }
+main();
